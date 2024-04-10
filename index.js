4f9b06ab4cc745562d182922b2eeff771d18e651
tarunpalsingh188 <tarunpalsingh188@gmail.com>
2024-04-10 23:22:59 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+require('dotenv').config()
+ const express = require('express')
+
+ const app = express()
+
+ const port = 4000
+
+ app.get('/',(req , res)=>{
+    res.send("Hello World")
+ })
+
+
+ app.get('/twitter',(req , res )=>{
+    res.send("tarundotcom")
+ })
+
+ app.get('/login', (req , res)=>{
+    res.send('<h1>Please login a profile</h1>')
+ })
+
+ app.get('/youtube', (req , res)=>{
+    res.send('<h2>Tarun Baghel</h2>')
+ })
+
+ app.listen(process.env.PORT,()=>{
+    console.log(`App listening on port ${port}`)
+ })
